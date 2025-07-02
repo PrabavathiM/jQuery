8c1531f00b838c90fb19154f07db31008e67c1b2
Prabavthi M <pprabavthi539@gmail.com>
2025-07-02 18:40:40 +0530

    server side processing added

--- DataTable-Practice/ssp.js
+++ DataTable-Practice/ssp.js
@@ -3,7 +3,14 @@ $(document).ready(function (){
     processing: true,
     serverSide: true,
     paging: true,
-    searching: false,
-    ordering: true,
-    })
-})
+    pagingType:'full_numbers',
+    pageLength:5,
+    lengthMenu: [5, 10, 15, 20, 25, 30, 35, 40, 45, 50],
+    
+    ajax: {
+        url: 'ssp.php',
+        type: 'POST',
+        dataType: "json"
+    }
+    });
+});
--- DataTable-Practice/ssp.php
+++ DataTable-Practice/ssp.php
@@ -1,38 +1,40 @@
 <?php 
-require( 'ssp.class.php');
-$table = 'employee';
-$primaryKey = 'id';
-$columns = array(
-    array( 'db' => 'first name', 'dt'=>0),
-    array( 'db' => 'last name', 'dt'=>1),
-    array( 'db' => 'position', 'dt'=>2),
-    array( 'db' => 'office', 'dt'=>3),
-    array(
-         'db' => 'start_date ',
-        'dt'=>4,
-        'formatter' => function($d, $row){
-            return date( 'jS M y', strtotime($d)); 
-        }
-         
-    ),
-    array(
-        'db'        => 'salary',
-        'dt'        => 5,
-        'formatter' => function( $d, $row ) {
-            return '$'.number_format($d);
-        }
-    )
-);
+$conn = new mysqli("localhost","root","","ajax");
+if($conn->connect_error) {
+    die ("connection failed:". $conn->connect_error);
+}
+// $conn->close();
+$draw = intval($_POST['draw']);
+$start = intval($_POST['start']);
+$length = intval($_POST['length']);
 
-$sql_details = array(
-    'user' => 'root',
-    'pass' => '',
-    'db'   => 'ajax',
-    'host' => 'localhost'
-);
+$totalQuery = $conn->query("SELECT COUNT(*) As total FROM employee");
+$totalRecords = $totalQuery->fetch_assoc() ['total'];
+$sql = "SELECT * FROM employee LIMIT $start, $length";
+$result = $conn->query($sql);
+// $sql_data= "SELECT * FROM employee";
+// $result = $conn->query($sql_data);
+$data = [];
+while ($row = $result->fetch_assoc()){
+    $data[] = [
+        $row ['first_name'],
+         $row ['last_name'],
+          $row ['office'],
+           $row ['start_date'],
+            $row ['salary'],
 
-echo json_encode(
-    SSP::simple( $_GET, $sql_details, $table, $primaryKey, $columns )
-);
+    ];
+}
+
+$response = [
+    "draw" => $draw,
+    "totalRecords" => $totalRecords,
+    "recordsFiltered" => $totalRecords,
+    "data" => $data
+];
+
+header('Content-Type: application/json');
+echo json_encode($response);
+
 
 ?>
