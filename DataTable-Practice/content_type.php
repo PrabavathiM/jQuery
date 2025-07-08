c6b5877ccc4cf9bdc0f3f6a286a80c98261e02c2
Prabavthi M <pprabavthi539@gmail.com>
2025-07-08 13:01:27 +0530

    total code update

--- DataTable-Practice/content_type.php
+++ DataTable-Practice/content_type.php
@@ -1,25 +1,4 @@
 <?php
 
-$data = [
-    ["Name", "Email", "Age"],
-    ["Alice", "alice@example.com", 25],
-    ["Bob", "bob@example.com", 30],
-    ["preethi", "preethi@gmail.com"]
-];
-// update code to the praba branch code update to now
-
-
-header('Content-Type: text/csv');
-header('Content-Disposition: attachment; filename="data.csv"');
-header('Pragma: no-cache');
-header('Expires: 0');
-
-$output = fopen('php://output', 'w');
-
-foreach ($data as $row) {
-    fputcsv($output, $row);
-}
-
-fclose($output);
-exit;
+echo "prabavathi manivannan";
 ?>
